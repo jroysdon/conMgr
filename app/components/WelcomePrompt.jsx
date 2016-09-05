63aa7f5c7cda38bd0ba3039bce20aa48666fac93
James Roysdon <jroysdon@Jamess-MBP.fios-router.home>
2016-09-04 23:44:08 -0400

    Add Parse Login - still not working

---
+++ app/components/WelcomePrompt.jsx
@@ -0,0 +1,52 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Parse = require('Parse');
+Parse.initialize("cmparseserver");
+Parse.serverURL = "https://cmparseserver.herokuapp.com/parse";
+
+
+// var welcomePrompt = "";
+//
+// Parse.User.logIn("jroysdon@gmail.com" , "12345678").then (function (user){
+//   welcomePrompt = Parse.User.current();
+//   }, function error (err){
+//       console.error(err.message);
+//   }
+// );
+var currentUser = Parse.User.current();
+var loginMenu = "";
+debugger;
+if (currentUser) {
+    loginMenu = "Welcome " + currentUser.get("nameFirst");
+  } else {
+    loginMenu = '<Link to=\"/Login\" activeClassName=\"active-link\">Login</Link>'
+  };
+var WelcomePrompt = React.createClass( {
+  getDefaultProps: function (){
+    return{
+     loginMenu: '<Link to=\"/Login\" activeClassName=\"active-link\">Login</Link>'
+        // loginMenu: 'Hi'
+     }
+  },
+  getInitialState: function (){
+    return {
+     loginMenu: this.props.loginMenu
+      //  loginMenu: '<Link to=\"/Login\" activeClassName=\"active-link\">Login</Link>'
+    }
+ },
+  handleNewData: function (updates) {
+    this.setState(updates);
+  },
+
+  render: function() {
+// var loginMenu = '<Link to=\"/Login\" activeClassName=\"active-link\">Login</Link>';
+   var loginMenu = this.state.loginMenu;
+    return (
+      <div ref="loginMenu" >
+        {loginMenu}{currentUser.get("nameFirst")}
+      </div>
+    )}
+});
+
+module.exports = WelcomePrompt;
